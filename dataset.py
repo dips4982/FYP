633c55c116c3073d5ecd01282d247941902cfd68
Srujan R <48976001+srujrs@users.noreply.github.com>
2021-11-16 15:52:49 +0530

    part-1

---
+++ dataset.py
@@ -0,0 +1,36 @@
+import os
+
+def download_dataset(dataset_type = "images", dataset_part = "train"):
+    if dataset_type == "images":
+        if dataset_part == "train":
+            if os.path.isdir("train2014"):
+                print("Train dataset for Images already present")
+            else:
+                os.system('wget http://images.cocodataset.org/zips/train2014.zip')
+                os.system('unzip train2014.zip')
+                os.system('rm train2014.zip')
+        else:
+            if os.path.isdir("val2014"):
+                print("Val dataset for Images already present")
+            else:
+                os.system('wget http://images.cocodataset.org/zips/val2014.zip')
+                os.system('unzip val2014.zip')
+                os.system('rm val2014.zip')
+
+    elif dataset_type == "questions":
+        if dataset_part == "train":
+            if os.path.isdir("v2_Questions_Train_mscoco"):
+                print("Train dataset for Questions already present")
+            else:
+                os.system('wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip')
+                os.system('unzip v2_Questions_Train_mscoco.zip')
+                os.system('rm v2_Questions_Train_mscoco.zip')
+        else:
+            if os.path.isdir("v2_Questions_Val_mscoco"):
+                print("Val dataset for Questions already present")
+            else:
+                os.system('wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip')
+                os.system('unzip v2_Questions_Val_mscoco.zip')
+                os.system('rm v2_Questions_Val_mscoco.zip')
+
+        
---
+++ train.py
@@ -0,0 +1,14 @@
+import dataset
+import extract_img_features as ex_img
+
+directories = {
+    "img_train" : "train2014",
+    "img_val" : "val2014",
+    "ques_train" : "v2_Questions_Train_mscoco",
+    "ques_val" : "v2_Questions_Val_mscoco"
+}
+
+dataset.download_dataset("Images", "train")
+dataset.download_dataset("Questions", "train")
+
+ex_img.ImgExtractor().extract(directories["img_train"])
