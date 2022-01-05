ff976ac077846e668dfd400833441872677ea788
yashm1 <yamantri007@gmail.com>
2022-01-05 23:20:14 +0530

    Text features changes

--- extract_text_features.py
+++ extract_text_features.py
@@ -12,11 +12,21 @@ class TextFeatureExtractor():
 
     def extract_features(self, direc):
         classifier = self.classifier
-        with open('answers.json') as data_file:    
+        with open('questions.json') as data_file:    
             with h5py.File('text_features_train.hdf5', 'w') as h5file:
                 data = json.load(data_file)
                 for questions in data['questions']:
                     h5file[str(questions['question_id'])] = classifier.encode(questions['question'])
-
+                    
+                    
+    def load_features(self):
+            fileh5 = h5py.File('text_features_train.hdf5', 'r')
+        item = fileh5['item']
+        keys = item.keys()
+        out_dict = {}
+        for key in keys:
+            out_dict[key] = item[key][()]
+        fileh5.close()
+        return out_dict
             
     
