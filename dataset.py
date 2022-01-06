229fbf71c74a88d4adb56651088309d65e1c7b66
srujrs <48976001+srujrs@users.noreply.github.com>
2022-01-06 14:52:24 +0530

    isfile

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ import os
 
 def download_dataset(cwd, dataset_type = "images", dataset_part = "train"):
     if not os.path.isdir("data"):
-        os.system('mkdir cwd/data')
+        os.system('mkdir ' + cwd + '/data')
 
     os.system('cd data')
 
@@ -24,28 +24,28 @@ def download_dataset(cwd, dataset_type = "images", dataset_part = "train"):
 
     elif dataset_type == "questions":
         if dataset_part == "train":
-            if os.path.isdir("v2_Questions_Train_mscoco"):
+            if os.path.isfile("v2_Questions_Train_mscoco.json"):
                 print("Train dataset for Questions already present")
             else:
                 os.system('wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip')
                 os.system('unzip v2_Questions_Train_mscoco.zip')
                 os.system('rm v2_Questions_Train_mscoco.zip')
 
-            if os.path.isdir("v2_Annotations_Train_mscoco"):
+            if os.path.isfile("v2_Annotations_Train_mscoco.json"):
                 print("Train dataset for Annotations already present")
             else:
                 os.system('wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip')
                 os.system('unzip v2_Annotations_Train_mscoco.zip')
                 os.system('rm v2_Annotations_Train_mscoco.zip')
         else:
-            if os.path.isdir("v2_Questions_Val_mscoco"):
+            if os.path.isfile("v2_Questions_Val_mscoco.json"):
                 print("Val dataset for Questions already present")
             else:
                 os.system('wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip')
                 os.system('unzip v2_Questions_Val_mscoco.zip')
                 os.system('rm v2_Questions_Val_mscoco.zip')
 
-            if os.path.isdir("v2_Annotations_Val_mscoco"):
+            if os.path.isfile("v2_Annotations_Val_mscoco.json"):
                 print("Val dataset for Annotations already present")
             else:
                 os.system('wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip')
